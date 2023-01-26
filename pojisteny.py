a38b18287449238bb8db1365a4ab5fe7d16c07d5
Tomáš Juránek <78249543+tomaash25@users.noreply.github.com>
2023-01-26 14:55:06 +0100

    Update pojisteny.py

--- pojisteny.py
+++ pojisteny.py
@@ -7,7 +7,7 @@ class Pojisteny:
 
     def __init__(self, jmeno, prijmeni, vek, telefon):
         """
-        konstruktor
+        konstruktor pro vytvoreni osoby
         :param jmeno:bude ulozen s velkymi pismeny kvuli vyhledavani
         :param prijmeni:bude ulozen s velkymi pismeny kvuli vyhledavani
         :param vek:
@@ -20,5 +20,6 @@ class Pojisteny:
 
     def __str__(self):
         return f"{self.jmeno} {self.prijmeni} ,věk:{self.vek}, telefonní číslo:{self.telefon}"
-
-
+        """ 
+        metoda vraci textovou podobu objektu
+        """
